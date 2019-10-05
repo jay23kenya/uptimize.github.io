c641c1d906edf913e52f93aa2331fc4889d527f2
Wade Smith <wadealsmith@gmail.com>
2019-10-05 14:27:05 -0400

    Routes and cdns

---
+++ uptimize/src/router.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import {
+  Route,
+  Redirect,
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
+import LandingPage from "./views/LandingPage/LandingPage";
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
