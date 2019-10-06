acdae5a31fedba1944e0f034704acceb88253ec0
Wade Smith <wadealsmith@gmail.com>
2019-10-06 19:09:37 -0400

    Nav changes

--- uptimize/src/components/Nav/Nav.js
+++ uptimize/src/components/Nav/Nav.js
@@ -1,27 +1,35 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav as Navigation,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
-} from "reactstrap";
+// import * as _ from "underscore";
+import NavContent from "./NavContent";
+
+
+const styles = {
+  whiteBackground: {
+    backgroundColor: "white",
+    height: "100px"
+  },
+  transparentBackground: {
+    backgroundColor: "rgb(256,256, 256, .8)",
+    height: "100px"
+  },
+  navbar: {
+    width: "100%",
+    height: "100px"
+  },
+};
 
 export class Nav extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      collapsed: true
+      collapsed: false,
+      transparent: false
     };
     this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.setTransparent = this.setTransparent.bind(this);
+    this.handleScroll = this.handleScroll.bind(this)
   }
 
   toggleNavbar() {
@@ -29,47 +37,54 @@ export class Nav extends Component {
       collapsed: !this.state.collapsed
     });
   }
+  componentDidMount = () => {
+    window.addEventListener("scroll", () => this.handleScroll());
+  };
+
+  handleScroll = () => {
+    var nav = document.getElementById("nav");
+    var stickLocation = document.getElementById("navStick").offsetTop;
+    if (window.pageYOffset > stickLocation) {
+      nav.classList.add("fixed-top");
+      this.setTransparent(true);
+    } else {
+      nav.classList.remove("fixed-top");
+      this.setTransparent(false);
+    }
+  };
+
+  setTransparent(t) {
+    if (t) {
+      this.setState({
+        transparent: true
+      });
+    } else {
+      this.setState({
+        transparent: false
+      });
+    }
+  }
 
   render() {
-    window.onscroll = function() {
-      var nav = document.getElementById("nav");
-      var stickLocation = document.getElementById("navStick").offsetTop;
-      if (window.pageYOffset > stickLocation) {
-        nav.classList.add("fixed-top");
-      } else {
-        nav.classList.remove("fixed-top");
-      }
-    };
-
     return (
       <div id="nav">
-        <Navbar sticky="top" color="light" light expand="md">
-          <NavbarBrand href="/">Uptimize</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Navigation className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">
-                  GitHub
-                </NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>Option 1</DropdownItem>
-                  <DropdownItem>Option 2</DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>Reset</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Navigation>
-          </Collapse>
-        </Navbar>
+        {this.state.transparent ? (
+          <div style={styles.transparentBackground}>
+            <NavContent
+              collapsed={this.state.collapsed}
+              toggleNavbar={this.toggleNavbar}
+              mainStyle={this.props.mainStyle}
+            />
+          </div>
+        ) : (
+          <div style={styles.whiteBackground}>
+            <NavContent
+              collapsed={this.state.collapsed}
+              toggleNavbar={this.toggleNavbar}
+              mainStyle={this.props.mainStyle}
+            />
+          </div>
+        )}
       </div>
     );
   }
