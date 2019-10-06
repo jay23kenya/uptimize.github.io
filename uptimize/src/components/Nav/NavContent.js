acda7bbe3be26b54a3612ca1a1d5407aa9ef23fa
Wade Smith <wadealsmith@gmail.com>
2019-10-06 19:08:19 -0400

    Working on nav

---
+++ uptimize/src/components/Nav/NavContent.js
@@ -0,0 +1,96 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav as Navigation,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+
+import { ReactComponent as Logo } from "../../images/logo.svg";
+import { Container } from "@material-ui/core";
+
+const styles = {
+  logo: {
+    height: "75%",
+    width: "75%",
+    position: {
+      position: "absolute",
+      top: 12.5
+    }
+  },
+  nav: {
+    backgroundColor: "grey",
+    padding: "15px",
+    marginLeft: "auto",
+    marginBottom: "auto",
+    marginTop: "auto"
+  },
+  bigNav: {
+    padding: "15px",
+    marginLeft: "auto",
+    marginBottom: "auto",
+    marginTop: "auto"
+  }
+};
+
+const NavContent = props => {
+  return (
+    <Navbar className=" h-100" color sticky="top" expand="md">
+      <Container fixed>
+        <div className="d-flex">
+          <div>
+            <NavbarBrand href="/">
+              <div style={styles.logo.position}>
+                <Logo height={styles.logo.height} width={styles.logo.width} />
+              </div>
+            </NavbarBrand>
+          </div>
+
+          <div style={styles.nav}>
+            <NavbarToggler onClick={props.toggleNavbar} />
+
+            <Collapse isOpen={props.collapsed} navbar>
+              <Navigation className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/components/">Components</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://github.com/reactstrap/reactstrap">
+                    GitHub
+                  </NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Options
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>Option 1</DropdownItem>
+                    <DropdownItem>Option 2</DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>Reset</DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Navigation>
+            </Collapse>
+          </div>
+        </div>
+      </Container>
+    </Navbar>
+  );
+};
+
+NavContent.propTypes = {
+  mainStyle: PropTypes.object,
+  toggleNavbar: PropTypes.func.isRequired,
+  collapsed: PropTypes.bool.isRequired
+};
+
+export default NavContent;
